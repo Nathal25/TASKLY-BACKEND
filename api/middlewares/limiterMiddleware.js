bcb89e6ab4da5614feb108a2155f1e88a2cce6fd
Nathalia Ortiz Granada <146957027+Nathal25@users.noreply.github.com>
2025-09-10 12:23:33 -0500

    Correccion en archivos

--- api/middlewares/limiterMiddleware.js
+++ api/middlewares/limiterMiddleware.js
@@ -1,20 +1,21 @@
-const jwt = require("jsonwebtoken");
+const rateLimit = require('express-rate-limit')
 
-// Middleware to authenticate JWT tokens
-function authenticateToken(req, res, next) {
-    // Get token from cookies
-    const token =  req.cookies.token;
+// Define the login rate limiter middleware
+const loginLimiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 5, // Limit each IP to 5 requests per windowMs (per 10 minutes) 
+    message: {
+        message: "Too many requests, please try again after 10 minutes"
+    },
 
-    //Verify if token exists
-    if (!token) return res.status(401).json({message: "No token provided"});
+    /**
+    Return rate limit info in the headers 
+    - RateLimit-Limit: Maximum number of requests allowed
+    - RateLimit-Remaining: How many requests are allowed before reaching the limit
+    - RateLimit-Reset: Time in seconds until the window restarts
+    */
+    standardHeaders: true,
+    legacyHeaders: false, // Disable the old headers
+})
 
-    try{
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET); // decodedToken contains the payload (when the token was created)
-        req.userId = decodedToken.userID; // Save userID in the request for further use (in other routes)
-        next(); // Call the next middleware or route body
-    }catch(error){
-        return res.status(401).json({message: "Invalid or expired token"});
-    }
-}
-
-module.exports = authenticateToken;
+module.exports = loginLimiter;
